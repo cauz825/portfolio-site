3daada110d5b110a7e3c34f56c9da74420069a3f
MCauz <michaelhcausey@gmail.com>
2021-02-05 17:23:26 -0800

    carousel installed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Navi from './Components/Navi'
+import HomeCarousel from './Components/HomeCarousel'
 
 function App() {
   return (
     <div>
       <Navi />
-      Components<br></br>
-        -Resume<br></br>
-        -Contact<br></br>
-        -Links<br></br>
+      <HomeCarousel />
     </div>
   );
 }
--- src/Components/Carousel.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function HomeCarousel() {
-
-}
-
-export default HomeCarousel;
