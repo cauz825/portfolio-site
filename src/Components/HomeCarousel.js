53cec8fea7877622fec3bf6425555c0a734b235f
MCauz <michaelhcausey@gmail.com>
2021-02-05 17:30:06 -0800

    carousel progress

--- src/Components/HomeCarousel.js
+++ src/Components/HomeCarousel.js
@@ -7,7 +7,7 @@ function HomeCarousel() {
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
+      src="https://a.espncdn.com/i/teamlogos/nfl/500/lac.png"
       alt="First slide"
     />
     <Carousel.Caption>
