83ad0e7d97fa2d0322b8bcc6ee708d9d79aa62aa
MCauz <michaelhcausey@gmail.com>
2021-02-14 21:20:52 -0800

    weekend update

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Document } from 'react-pdf';
-import ResumeImg from '../Images/resume.jpg'
+// import { Document } from 'react-pdf'
+import ResumeImg from '../Images/resume.jpg';
+// import ResumePdf from '../Images/resume.pdf'
 
 function Resume() {
     return (
         <div>
             <center><img src={ResumeImg} alt='' /></center>
-            <Document file='src/Images/resume.pdf' />
+            {/* <center><Document file={ResumePdf} onLoadError={console.error} /></center> */}
         </div>
     )
 }
