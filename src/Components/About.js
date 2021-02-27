3ec215a8080ad16ef83d51c177fd0c327b691b3b
MCauz <michaelhcausey@gmail.com>
2021-02-27 14:59:09 -0800

    updates to about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,6 +5,9 @@ function About() {
     return(
         <div>
             <center><img src={headshot} alt='' width='300' ></img></center>
+            <p>
+            An enthusiastic, conscientious, and solution-oriented professional. Recent graduate from the Flatiron School’s Software Engineering Program. Extensive experience in large- scale national and international production and process management. Seeking a work environment that allows employees to thrive and advance in their field as well as a company culture that fosters their employees’ continued growth and development.
+            </p>
         </div>
     )
 
