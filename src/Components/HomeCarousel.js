9e623370570501ac5460d6211d7adafe2b92ceb2
MCauz <michaelhcausey@gmail.com>
2021-03-05 11:13:55 -0800

    more carousel tweaks

--- src/Components/HomeCarousel.js
+++ src/Components/HomeCarousel.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap'
 import Headshot from '../Images/headshot.jpg'
+import Laptop from '../Images/coding-laptop.jpg'
 
 function HomeCarousel() {
     return(
@@ -8,12 +9,10 @@ function HomeCarousel() {
   <Carousel.Item>
     <center>
       <img
-      className="d-block w-100"
-      src={Headshot}
-      alt="First slide"
-      height='800'
-      width='400'
-      />
+      className="d-block w-98"
+      src={Laptop}
+      alt="First slide" />
+
       <Carousel.Caption>
         <h3>Welcome to my portfolio site</h3>
         {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
@@ -22,27 +21,17 @@ function HomeCarousel() {
   </Carousel.Item>
       
   <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
-      alt="Third slide"
-    />
+    <center>
+      <img
+        className="d-block w-98"
+        src={Headshot}
+        alt="Second slide" 
+        href='/about' />
+    </center> 
 
     <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <h3>A Little About Me</h3>
+      {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
