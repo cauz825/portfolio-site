0c4440e30cc41f19acd0d375c74b0f677797553f
MCauz <michaelhcausey@gmail.com>
2021-03-04 19:00:56 -0800

    removed pdf

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,13 +1,10 @@
 import React from 'react';
-// import { Document } from 'react-pdf'
 import ResumeImg from '../Images/resume.jpg';
-// import ResumePdf from '../Images/resume.pdf'
 
 function Resume() {
     return (
         <div>
             <center><img src={ResumeImg} alt='' /></center>
-            {/* <center><Document file={ResumePdf} onLoadError={console.error} /></center> */}
         </div>
     )
 }
