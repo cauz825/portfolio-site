d020d59ab10d90f0cb603bd21a1bc7c231aa4064
MCauz <michaelhcausey@gmail.com>
2021-03-14 20:34:38 -0700

    second paragraph, need content

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,9 +6,8 @@ function About() {
         <div>
             <center>
                 <img src={headshot} alt='' width='300' ></img>
-                <p>
-                An enthusiastic, conscientious, and solution-oriented professional. Recent graduate from the Flatiron School’s Software Engineering Program. Extensive experience in large- scale national and international production and process management. Seeking a work environment that allows employees to thrive and advance in their field as well as a company culture that fosters their employees’ continued growth and development.
-                </p>
+                <p>An enthusiastic, conscientious, and solution-oriented professional. Recent graduate from the Flatiron School’s Software Engineering Program. Extensive experience in large- scale national and international production and process management. Seeking a work environment that allows employees to thrive and advance in their field as well as a company culture that fosters their employees’ continued growth and development.</p><br></br>
+                <p>[Insert Personal Anecdote]</p>
             </center>
         </div>
     )
